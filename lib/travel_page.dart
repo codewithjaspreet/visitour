f3e34a846da7228200943508c3225ffe941dc1b2
Jaspreet Singh Sodhi <jaspreetsodhi02@gmail.com>
2022-04-15 14:37:30 +0530

    HStack -> row , Vstack - > column tabs added

--- lib/travel_page.dart
+++ lib/travel_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import "package:velocity_x/velocity_x.dart";
 
@@ -8,8 +9,17 @@ class TravelPage extends StatefulWidget {
   State<TravelPage> createState() => _TravelPageState();
 }
 
-class _TravelPageState extends State<TravelPage> {
+class _TravelPageState extends State<TravelPage> with TickerProviderStateMixin {
   final image = 'https://avatars.githubusercontent.com/u/85099922?v=4';
+  late TabController tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    tabController = TabController(length: 4, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -68,6 +78,78 @@ class _TravelPageState extends State<TravelPage> {
         ].vStack())
             .makeCentered()
             .h32(context),
+        ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+          child: VxBox(
+            child: VStack([
+              TabBar(
+                controller: tabController,
+                indicatorColor: Colors.purple,
+                indicatorSize: TabBarIndicatorSize.label,
+                labelColor: Vx.purple500,
+                unselectedLabelColor: Vx.gray400,
+                labelPadding: Vx.m16,
+                tabs: [
+                  Icon(
+                    Icons.map,
+                    size: 30,
+                  ),
+                  Icon(
+                    Icons.pin_drop,
+                    size: 30,
+                  ),
+                  Icon(
+                    Icons.restaurant,
+                    size: 30,
+                  ),
+                  Icon(
+                    Icons.person,
+                    size: 30,
+                  ),
+                ],
+              ),
+              TabBarView(
+                children: ["1", "2", "3", "4"]
+                    .map((e) => VStack([
+                          "Discover Places in London"
+                              .text
+                              .gray600
+                              .xl2
+                              .bold
+                              .make(),
+                          HStack([
+                            Image.network(image)
+                                .wh(context.percentWidth * 35, 100)
+                                .cornerRadius(10),
+                            20.widthBox,
+                            VStack(
+                              [
+                                "Tower Bridge".text.semiBold.make(),
+                                3.heightBox,
+                                "Southwork".text.make().shimmer(),
+                                5.heightBox,
+                                HStack([
+                                  VxRating(
+                                    onRatingUpdate: (value) {},
+                                    size: 13,
+                                  ),
+                                  5.heightBox,
+                                  "(100)".text.xs.gray600.make(),
+                                ])
+                              ],
+                              crossAlignment: CrossAxisAlignment.start,
+                            ).expand()
+                          ]).cornerRadius(8).backgroundColor(Vx.gray200)
+                        ]).p16())
+                    .toList(),
+                controller: tabController,
+              ).expand()
+            ]),
+          ).white.make(),
+        ).expand(),
       ]),
     );
   }
